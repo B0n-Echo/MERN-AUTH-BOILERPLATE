f24bc33a2dd17a93273af6b308b0cd0cbde0a0a1
NikhileshP <nikhileshhpandey@gmail.com>
2022-05-26 10:35:50 +0530

    🔨Add middleware and Scripts

--- server/server.js
+++ server/server.js
@@ -1,13 +1,29 @@
 const express = require('express');
+const authRoutes = require('./routes/auth');
+const morgan = require('morgan');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
 
 const app = express();
 
-app.get('/api/signup', (req, res) => {
-    res.json({
-        data: `you have hit the signup endpoint`
-    });
-});
+//* APP MIDDLEWARE
+app.use(morgan('dev'));
+app.use(bodyParser.json());
 
+if(process.env.NODE_ENV !== 'production') {
+    app.use(cors({origins:`http://localhost:3000`} )); //* Allow all origins to access
+}
+
+
+
+//* ROUTES
+app.use(`/api`, authRoutes)
+
+
+//* SERVER
 const port = process.env.PORT || 8000;
 app.listen(port, ()=> {
     console.log(`API is Up and running on port ${port}`);
