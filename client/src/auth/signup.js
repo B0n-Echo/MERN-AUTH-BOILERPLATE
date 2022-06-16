b1a872ea43d502c9a6dff937402618115e9067c2
NikhileshP <nikhileshhpandey@gmail.com>
2022-06-16 09:38:17 +0530

    🛂MERN_AUTH08: REACT SIGN UP Setup - basic Signup Setup

--- client/src/auth/signup.js
+++ client/src/auth/signup.js
@@ -4,10 +4,56 @@ import Layout from '../core/Layout';
 import {ToastContainer, toast} from 'react-toastify';
 
 const Signup = () => {
-   return ( <Layout>
+
+    const [values, setValues] = useState({
+        name: '',
+        email: '',
+        password: '',
+        buttonText: 'Submit'   //* dynamically change the button text
+    });
+
+
+    const {buttonText} = values;
+
+
+    const signupForm = () => (
+        <form>
+            <div className="form-group">
+                <lable className="text-muted">Name</lable>
+                <input onChange={handleChange('name')}  type="text" className="form-control"/>
+            </div>
+          
+            <div className="form-group">
+                <lable className="text-muted">Email</lable>
+                <input onChange={handleChange('email')}  type="email" className="form-control"/>
+            </div>
+          
+            <div className="form-group">
+                <lable className="text-muted">Password</lable>
+                <input onChange={handleChange('password')}  type="password" className="form-control"/>
+            </div>
+            
+            <div>
+                <button className="btn btn-primary" onClick={clickSubmit()}>{buttonText}</button>
+            </div>
+        </form>
+    );
+
+    const handleChange = (name) => (event) => {
+        // event.target.value;
+    }
+
+    const clickSubmit = (event) => {
+
+    }
+
+   return ( 
+   <Layout>
         <ToastContainer  />
         <h1>SignUp</h1>
-    </Layout>)
+        {signupForm()}
+    </Layout>
+    )
 }
 
 export default Signup;
