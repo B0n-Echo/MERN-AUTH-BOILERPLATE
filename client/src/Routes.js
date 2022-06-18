cb57827d4cbdad17cf19f6fd8ecec173c266d2c8
NikhileshP <nikhileshhpandey@gmail.com>
2022-06-18 10:33:12 +0530

    🚧MERN_AUTH09: SIGN IN Setup

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -1,13 +1,15 @@
 import { BrowserRouter, Switch ,Route } from "react-router-dom";
 import App from "./App";
 import Signup from "./auth/signup";
+import Signin from "./auth/signin";
 
 const Routes = () => {
     return (
         <BrowserRouter>
         <Switch>
             <Route path="/"  exact component={App} />
-            <Route path="/signup"  component={Signup} />
+            <Route path="/signup"  exact component={Signup} />
+            <Route path="/signin"  exact component={Signin} />
         </Switch>
         </BrowserRouter>
     )
